e86b9486f7d3b63ae21e3e82176322b2111da1ba
maddenlockin <maddenlockin@gmail.com>
2021-12-06 15:58:44 -0800

    all tests passing

--- src/components/Display/Display.test.jsx
+++ src/components/Display/Display.test.jsx
@@ -1,11 +1,12 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Display from "./Display";
 
 it("should render Display component", () => {
     const { container } = render(
         <Display
-        judgements={["ewww", "blah", "ugh"]}
+        judgementArr={["ewww", "blah", "ugh"]}
         />
     );
+    screen.debug();
     expect(container).toMatchSnapshot();
 });
